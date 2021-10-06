2e4878cbc03aed82f9a017bd9c24e80cababb914
Kostushka <nastyatery47@yandex.ru>
2021-10-06 19:06:47 +0300

    Created file for constants with constants for api

---
+++ src/constants/api.js
@@ -0,0 +1,19 @@
+// API FIRST
+export const API_ROOT = 'https://www.breakingbadapi.com/api/';
+export const API_CHARACTERS = 'characters';
+
+// API SECOND
+
+// // common api
+// export const HTTP = 'http://';
+// export const HTTPS = 'https://';
+
+// // base api
+// export const API_ROOT = 'https://swapi.dev/api/';
+// export const API_PEOPLE = 'people';
+// export const API = 'swapi.dev/api/';
+
+// // visual api
+// export const GUIDE_URL_IMG = 'https://starwars-visualguide.com/assets/img/';
+// export const GUIDE_PEOPLE = 'characters';
+// export const GUIDE_EXTENSION = '.jpg';
