426145b27e4674baacb9e3a81dad792c48247977
Kostushka <nastyatery47@yandex.ru>
2021-10-07 21:44:01 +0300

    Added component for handling request error

--- src/containers/PeoplePageContainer/PeoplePageContainer.jsx
+++ src/containers/PeoplePageContainer/PeoplePageContainer.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from 'react';
 import { API_CHARACTERS } from '../../constants/api';
 import { getApiResource } from '../../utils/network';
+import { withErrorApi } from '../../hoc-helper/withErrorApi';
 // import { getPeopleId } from '../../services/getPeopleData';
 // import { getPeopleImage } from '../../services/getPeopleData';
 import styles from './PeoplePageContainer.module.css';
 import PeoplePage from '../../components/PeoplePage/PeoplePage';
 
-const PeoplePageContainer = () => {
+const PeoplePageContainer = ({ setErrorApi }) => {
     const [people, setPeople] = useState([]);
 
     const getResourse = async (url) => {
         const res = await getApiResource(url);
-        const peopleList = res.map(({ char_id, name, img }) => {
-            // const id = getPeopleId(url);
-            // const img = getPeopleImage(id);
-            return {
-                id: char_id,
-                name,
-                img,
-            };
-        });
-        setPeople(peopleList);
+        if (res) {
+            const peopleList = res.map(({ char_id, name, img }) => {
+                // const id = getPeopleId(url);
+                // const img = getPeopleImage(id);
+                return {
+                    id: char_id,
+                    name,
+                    img,
+                };
+            });
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
     };
 
     useEffect(() => {
@@ -30,4 +36,4 @@ const PeoplePageContainer = () => {
     return <>{people && <PeoplePage people={people} />}</>;
 };
 
-export default PeoplePageContainer;
+export default withErrorApi(PeoplePageContainer);
