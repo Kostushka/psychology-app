7a284b961ef7806a506fd29da17cb093604551d8
Kostushka <nastyatery47@yandex.ru>
2021-10-07 21:42:35 +0300

    Created HOC to handle request error

---
+++ src/hoc-helper/withErrorApi.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import ErrorMessage from '../components/ErrorMessage';
+
+export const withErrorApi = (Component) => (props) => {
+    const [errorApi, setErrorApi] = useState(false);
+    return (
+        <>
+            {errorApi ? (
+                <ErrorMessage />
+            ) : (
+                <Component setErrorApi={setErrorApi} {...props} />
+            )}
+        </>
+    );
+};
