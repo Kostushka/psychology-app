17c7a6381bae9a237385f5efaf35da75493403e8
Kostushka <nastyatery47@yandex.ru>
2021-10-09 20:23:42 +0300

    Added component for NavLink

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <ul className={styles.list__container}>
+                <li>
+                    <NavLink to='/' exact>
+                        Главная
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/people' exact>
+                        Люди
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
