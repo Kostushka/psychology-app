df4730f058ce7f8ae0103e066154121ca5b87f47
Kostushka <nastyatery47@yandex.ru>
2021-10-21 13:50:35 +0300

    Added icon of favorites

--- src/components/PersonPage/PersonPhoto/PersonPhoto.jsx
+++ src/components/PersonPage/PersonPhoto/PersonPhoto.jsx
@@ -1,7 +1,35 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { setPersonToFavorite, removePersonToFavorite } from '@store/actions';
+import favoriteFill from './img/favorite-fill.svg';
+import favorite from './img/favorite.svg';
 import styles from './PersonPhoto.module.css';
 
-const PersonPhoto = ({ personName, personPhoto }) => {
+const PersonPhoto = ({
+    personName,
+    personPhoto,
+    personId,
+    personFavorite,
+    setPersonFavorite,
+}) => {
+    const dispatch = useDispatch();
+
+    const dispatchFavoritePeople = () => {
+        if (personFavorite) {
+            dispatch(removePersonToFavorite(personId));
+            setPersonFavorite(false);
+        } else {
+            dispatch(
+                setPersonToFavorite({
+                    [personId]: {
+                        name: personName,
+                        img: personPhoto,
+                    },
+                })
+            );
+            setPersonFavorite(true);
+        }
+    };
     return (
         <>
             <div className={styles.container}>
@@ -9,6 +37,12 @@ const PersonPhoto = ({ personName, personPhoto }) => {
                     className={styles.photo}
                     src={personPhoto}
                     alt={personName}
+                />
+                <img
+                    className={styles.favorite}
+                    onClick={dispatchFavoritePeople}
+                    src={personFavorite ? favoriteFill : favorite}
+                    alt='Добавить в избранное'
                 />
             </div>
         </>
@@ -18,6 +52,9 @@ const PersonPhoto = ({ personName, personPhoto }) => {
 PersonPhoto.propTypes = {
     personPhoto: PropTypes.string,
     personName: PropTypes.string,
+    personId: PropTypes.string,
+    personFavorite: PropTypes.bool,
+    setPersonFavorite: PropTypes.func,
 };
 
 export default PersonPhoto;
--- src/containers/PersonPageContainer/PersonPageContainer.jsx
+++ src/containers/PersonPageContainer/PersonPageContainer.jsx
@@ -3,28 +3,35 @@ import React, { useEffect, useState } from 'react';
 import { getApiResource } from '@utils/network';
 import { API_PERSON } from '@constants/api';
 import { withErrorApi } from '@hoc-helper/withErrorApi';
-import PersonPhoto from '@components/PersonPage/PersonPhoto'
+import PersonPhoto from '@components/PersonPage/PersonPhoto';
 import PersonLinkBack from '@components/PersonPage/PersonLinkBack';
 import PersonDirection from '@components/PersonPage/PersonDirection';
 import UiLoading from '@ui/UiLoading';
 
 import styles from './PersonPageContainer.module.css';
+import { useSelector } from 'react-redux';
 
 const PersonInfo = React.lazy(() =>
     import('@components/PersonPage/PersonInfo')
 );
 
 const PersonPageContainer = ({ match, setErrorApi }) => {
+    const [personId, setPersonId] = useState(null);
     const [personInfo, setPersonInfo] = useState(null);
     const [personName, setPersonName] = useState(null);
     const [personPhoto, setPersonPhoto] = useState(null);
     const [personDirection, setPersonDirection] = useState(null);
+    const [personFavorite, setPersonFavorite] = useState(false);
+
+    const storeDate = useSelector((state) => state.favoriteReducer);
 
     useEffect(() => {
         (async () => {
             const id = match.params.id;
             const res = await getApiResource(`${API_PERSON}/${id}`);
             // const res = await getApiResource();
+            storeDate[id] ? setPersonFavorite(true) : setPersonFavorite(false);
+            setPersonId(id);
             if (res) {
                 // res.map(({ id, name, img, desc, info }) => {
                 //     const idPerson = Number(match.params.id);
@@ -77,6 +84,9 @@ const PersonPageContainer = ({ match, setErrorApi }) => {
                     <PersonPhoto
                         personPhoto={personPhoto}
                         personName={personName}
+                        personId={personId}
+                        personFavorite={personFavorite}
+                        setPersonFavorite={setPersonFavorite}
                     />
 
                     {personInfo && (
