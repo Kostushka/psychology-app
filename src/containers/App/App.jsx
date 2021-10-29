d207880b18f7df6a7ff8ef11aa0230c03089776d
Kostushka <nastyatery47@yandex.ru>
2021-10-29 09:36:02 +0300

    Deploy app

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import Header from '@components/Header';
 import routesConfig from '@routes/routesConfig';
+import { REPO_NAME } from '@constants/repo';
 
 import styles from './App.module.css';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={`/${REPO_NAME}/`}>
             <div className={styles.wrapper}>
                 <Header />
                 <Switch>
