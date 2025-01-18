abb642432cec2ca39cbdfaed7c1def6633628199
Kostushka <86732118+Kostushka@users.noreply.github.com>
2025-01-18 17:36:07 +0300

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
 export const API_ROOT =
-    'https://gist.githubusercontent.com/Kostushka/76ae7d98057b5b3800b50bf83a9925d9/raw/e42dc8eda622d6c5cb4da4bbac489a20f33c0746/Psy-catalog.js';
+    'https://gist.githubusercontent.com/Kostushka/76ae7d98057b5b3800b50bf83a9925d9/raw/03b3f20b55b523de7f8f58e6cca916331449d2ac/Psy-catalog.js';
